5483e8e16857c8c18a8f51b5b25cc433d007e9a2
Tan Dang <>
2022-05-21 11:02:58 +0700

    feat: update source code

--- wp-content/themes/nftdaddy/page-template/template-home.php
+++ wp-content/themes/nftdaddy/page-template/template-home.php
@@ -3,9 +3,14 @@
 get_header();
 
 ?>
-<div class="container md:pt-28 pt-28 md:pb-28 pb-14">
+<?php if (wp_is_mobile()) : ?>
+	<div class="innerpage-background bg-cover md:pt-40 pt-28 pb-7 md:pb-14 relative" style="background-image: url(<?php echo THEME_URL ?>/assets/images/hero-banner-bg.svg);">
+		<section class="innerpage-area"></section>
+	</div>
+<?php endif; ?>
+<div class="container md:pt-28 <?php echo !wp_is_mobile() ? 'pt-14' : '' ?> md:pb-28 pb-14">
 	<?php the_content();
-
 	?>
 </div>
+
 <?php get_footer(); ?>
